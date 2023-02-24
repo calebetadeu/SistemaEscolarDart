597e4a00f43ba3c3c4445eb344bffcba488bdca7
Calebe Tadeu Matos da Silva <calebe.silva@efrete.com.br>
2023-02-24 08:45:31 -0300

    Sistema Escola

---
+++ bin/sistema_escola_dart.dart
@@ -0,0 +1,5 @@
+import 'package:sistema_escola_dart/sistema_escola_dart.dart' as sistema_escola_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${sistema_escola_dart.calculate()}!');
+}
---
+++ test/sistema_escola_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:sistema_escola_dart/sistema_escola_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
